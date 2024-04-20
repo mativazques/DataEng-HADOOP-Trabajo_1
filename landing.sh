f9bed86bf055d6c14390c306ca069473bc56d4e9
mativazques <matirvazques@gmail.com>
2024-04-19 23:39:38 -0300

    correciones

--- landing.sh
+++ landing.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-# Descargar el .csv con -wget 
-wget -P /home/hadoop/landing https://raw.githubusercontent.com/fpineyro/homework-0/master/starwars.csv
+# Descargables 
 
-# Se coloca el .csv en hdfs, dentro de la carpeta de /ingest 
-hdfs dfs -put /home/hadoop/landing/starwars.csv /ingest 
+url="https://raw.githubusercontent.com/fpineyro/homework-0/master/starwars.csv"
+
+# Directorios 
+
+landing="/home/hadoop/landing"
+file="starwars.csv"
+
+# Se hace el wget con -O para sobreescribir el archivo descargado.
+
+wget -O "$landing"/"$file" "$url"
+
+# Se coloca el .csv en hdfs, dentro de la carpeta de /ingest
+# se utiliza -f para sobreescribir si ya existe el hdfs
+# con ese nombre. 
+
+hdfs dfs -put -f "$landing"/"$file" /ingest
 
 # Borrar el .csv de landing 
 rm /home/hadoop/landing/starwars.csv 
+
+
+
